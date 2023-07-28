0b14a40b1ece4f032601b87f64a847d6755f2eea
Pedro Henrique Rafael <pedro.rodrigues.rafael@gmail.com>
2023-07-28 17:38:20 -0300

    update script

--- 2. Análise de sentimentos/app.py
+++ 2. Análise de sentimentos/app.py
@@ -2,13 +2,13 @@ import os
 import gradio
 from models.predict_model import AnalyzeSentiment
 
+MODEL_DIR = '95866330256478990561605bbf7e39d6'
+MODEL = os.path.join(os.getcwd(), 'models', MODEL_DIR, 'artifacts', 'model.pkl')
+
 def gradio_wrapper(sentence):
     analizer = AnalyzeSentiment(MODEL)
     result = analizer.predict(sentence)
     return {result['sentiment'] : result['proba']}
-
-MODEL_DIR = '95866330256478990561605bbf7e39d6'
-MODEL = os.path.join(os.path.dirname(os.getcwd()), 'models', MODEL_DIR, 'artifacts', 'model.pkl')
 
 demo = gradio.Interface(
     fn=gradio_wrapper,
